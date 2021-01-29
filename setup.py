a109d04a465ba660ac4aeecce6d0ebb697d548d0
starskyC <cl2004_0123@aliyun.com>
2021-01-29 20:26:48 +0800

    Add github.com url to setup.py

--- setup.py
+++ setup.py
@@ -5,18 +5,19 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pipefun", # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="starskyC",
     author_email="cl2004_0123@aliyun.com",
-    description="A lightweighted package that contains pipe operators and functional tools",
+    description="A lightweight package that contains pipe operators and functional tools",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/starskyC/pipefun",
     packages=find_packages(),
+    license='MIT',
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.8',
 )
