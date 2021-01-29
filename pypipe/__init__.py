a197205ceaaa37925552794f28cd91c21ce6f9a7
小C <oncwnuOzLm9rTzT_45Oo3mO24IN8@git.weixin.qq.com>
2021-01-29 19:07:38 +0800

    upload

---
+++ pypipe/__init__.py
@@ -0,0 +1,2 @@
+from Functionals import _id, curry, apply, add, square, sqrt, reduce, mapFunc, filterFunc, compose, rcompose
+from Pipable import Pipable, out
--- tests/test.py
+++ tests/test.py
@@ -1,14 +1,15 @@
+from os import getcwd
 import sys
-sys.path.append('..')
+sys.path.append(getcwd())
 
 from pypipe.Pipable import Pipable, out
 from pypipe.Functionals import add, square, curry
 
 x = Pipable(3)
 
-add_currying = curry(2)(add)
+add_to = curry(2)(add)
 
-print( x >> add_currying(3) >> square >> out ) # 36
+print( x >> add_to(3) >> square >> out ) # 36
 
 x = Pipable(3)
 y = Pipable(5)
